8ef4d567ca976d1f8ab1a6babd6bba3382522e18
Pamarthi <nancharaiah.pamarthi@accenture.com>
2026-03-19 15:21:26 +0530

    created opportunity LWC

---
+++ force-app/main/default/lwc/webOpportunities/webOpportunities.js
@@ -0,0 +1,37 @@
+import { LightningElement, wire, track } from 'lwc';
+import getOpportunities from '@salesforce/apex/webOpportunitiesController.getOpportunities';
+
+const columns = [
+    { label: 'Opportunity Name', fieldName: 'Name', type: 'text' },
+    { label: 'Stage', fieldName: 'StageName', type: 'text' },
+    { label: 'Close Date', fieldName: 'CloseDate', type: 'date' },
+    { label: 'Amount', fieldName: 'Amount', type: 'currency' }
+];
+
+export default class WebOpportunities extends LightningElement {
+     allOpportunities = [];
+     filteredOpportunities = [];
+    searchKey = '';
+    columns = columns;
+
+    @wire(getOpportunities)
+    wiredOpportunities({ error, data }) {
+        if (data) {
+            this.allOpportunities = data;
+            this.filteredOpportunities = data;
+        } else if (error) {
+            console.error('Error fetching Opportunities:', error);
+        }
+    }
+
+    handleSearch(event) {
+        this.searchKey = event.target.value.toLowerCase();
+        if (this.searchKey) {
+            this.filteredOpportunities = this.allOpportunities.filter(opp =>
+                opp.Name.toLowerCase().includes(this.searchKey)
+            );
+        } else {
+            this.filteredOpportunities = this.allOpportunities;
+        }
+    }
+}
